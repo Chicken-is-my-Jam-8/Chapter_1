15b51daaf53ef4106df327e6b7a168ea3d037bb5
Chicken-is-my-Jam-8 <s1347556@monmouth.edu>
2024-09-13 16:12:27 -0400

    Update format_print.py

--- format_print.py
+++ format_print.py
@@ -1,12 +1,13 @@
-
 # Alexander J. Jackson
 # format_print.py 
 
-comb_shape = "+--+--+--+\n|  |  |  |"
-bottom_line = "+--+--+--+"
+num1 = 1.8
+num2 = 12.9
+num3 = 2
+num4 = 100.25
 
 print()
-print(comb_shape)
-print(comb_shape)
-print(comb_shape)
-print(bottom_line)
+print(f'{num1:>15,.2f}')
+print(f'{num2:>15,.2f}')
+print(f'{num3:>15,.2f}')
+print(f'{num4:>15,.2f}')
