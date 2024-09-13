384e5cb8c8a9f88d9a42221d0a94edc7d302b92f
Chicken-is-my-Jam-8 <s1347556@monmouth.edu>
2024-09-13 16:05:26 -0400

    Add files via upload

---
+++ print_grid.py
@@ -0,0 +1,12 @@
+
+# Alexander J. Jackson
+# format_print.py 
+
+comb_shape = "+--+--+--+\n|  |  |  |"
+bottom_line = "+--+--+--+"
+
+print()
+print(comb_shape)
+print(comb_shape)
+print(comb_shape)
+print(bottom_line)
